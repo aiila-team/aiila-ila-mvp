1fac3d635d17ba612977ff02e7ebac9c65d2e583
ChandraLikhithapalagiri <likhithapalagiri638@gmail.com>
2026-05-07 19:15:00 +0530

    Second commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,15 @@
 from fastapi import FastAPI
-from app.api.v1.auth import router as auth_router
+from loguru import logger
+
 from app.core.database import Base, engine
 from app.models.user import User
 
-# Create all database tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="ILA Backend",
     version="0.1.0"
 )
-
-app.include_router(
-    auth_router,
-    prefix="/api/v1/auth",
-    tags=["Authentication"]
-)
-
 
 @app.get("/api/v1/health")
 def health_check():
